16bae675302015177325b5997d0a6a59407eaf01
Bob <bobvanhooff@outlook.com>
2023-10-09 19:52:53 +0200

    Log splitting tool

---
+++ LogSplitter/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace LogSplitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FormatOutput(object sender, RoutedEventArgs e)
+        {
+            var names = new List<string>();
+
+            foreach (string name in this.names.Text.Split('\n'))
+            {
+                names.Add(name); 
+            }
+
+            output.Text = "";
+            foreach (string line in this.input.Text.Split('\n'))
+            {
+                foreach(string name in names)
+                {
+                    if (line.Trim().ToLower().Contains(name.Trim().ToLower()))
+                    {
+                        output.Text += line;
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # log-splitter
+
+This is a very simple tool I wrote back in 2017, made to extract (chat) logs regarding one or more people, subjects, or other keywords.  
+It's rather straightforward and simple (both code and usage), you paste your log in the left, keywords in the middle, click the button, and done.
+
